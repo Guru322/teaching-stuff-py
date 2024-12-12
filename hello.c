72ec808109d0974541322be290a76a51e6a14ede
GURU SENSEI <sahoogurucharan00@gmail.com>
2024-12-12 12:14:07 +0000

    commit from code space

---
+++ hello.c
@@ -0,0 +1,5 @@
+#include <stdio.h>  //This is called Header(In this case it is the standard I/O libraby)
+int main(){     //This the function main the function name 
+    printf("Hello someone \n"); // its the statement
+    return 0;  //return value
+}    // {} means start and end of your code 
